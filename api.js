4d9a8489660615cf8f1b6a5038c7bd35d5b8a7d7
Ivan Baluta <ltlaitoff@gmail.com>
2022-12-17 22:45:05 +0200

    feat: Added lang = ua in api

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const BASE_API_LINK = 'https://api.openweathermap.org/data/2.5'
 
 async function getWeatherInCity(city) {
-	const api = `${BASE_API_LINK}/weather?q=${city}&appid=${process.env.API_TOKEN}&units=metric`
+	const api = `${BASE_API_LINK}/weather?q=${city}&appid=${process.env.API_TOKEN}&units=metric&lang=ua`
 
 	const response = await fetch(api, {
 		method: 'get',
